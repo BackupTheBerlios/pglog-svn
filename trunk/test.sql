8fa75638607f6ec6b48ebdd334ec232b7428dc06
manlio-perillo <manlio-perillo@e738098c-ebc0-3944-8ebc-b0bb319865b5>
2006-05-10 18:36:46 +0000

    modified the example a bit

--- trunk/test.sql
+++ trunk/test.sql
@@ -1,11 +1,19 @@
---SELECT pglog.enable_log('baka');
+/*
+DROP TABLE test CASCADE;
+
+CREATE TABLE test (
+	x INTEGER PRIMARY KEY,
+        s TEXT
+); 
+*/
+
+--SELECT pglog.enable_log('test');
 --LISTEN pglog;
---TRUNCATE baka;
---INSERT INTO baka VALUES (1, 'baka');
---UPDATE baka SET s='horray' WHERE x = 1;
---DELETE FROM baka WHERE x = 1;
+--INSERT INTO test VALUES (1, 'test');
+--UPDATE test SET s='modified' WHERE x = 1;
+--DELETE FROM test WHERE x = 1;
 --SELECT * FROM pglog.Logs;
---SELECT * FROM baka;
---SELECT pglog.revert(5);
---SELECT pglog.disable_log('baka');
+--SELECT * FROM test;
+--SELECT pglog.revert(4);
+--SELECT pglog.disable_log('test');
 --SELECT pglog.last_change_id();

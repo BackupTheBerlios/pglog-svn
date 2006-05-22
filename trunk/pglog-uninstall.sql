3bac54ad942df4c360e77ef32ed05ad2feb92026
manlio-perillo <manlio-perillo@e738098c-ebc0-3944-8ebc-b0bb319865b5>
2006-05-22 09:44:33 +0000

    minor corrections for documentation added a tutorial in italian

--- trunk/pglog-uninstall.sql
+++ trunk/pglog-uninstall.sql
@@ -4,7 +4,7 @@ pglog: enable logs on a PostgreSQL database.
 $Id$
 
 THIS SOFTWARE IS UNDER MIT LICENSE.
-(C) 2006 Perillo Manlio (manlio.perillo@gmail.com)
+Copyright (c) 2006 Perillo Manlio (manlio.perillo@gmail.com)
 
 Read LICENSE file for more informations.
 */
